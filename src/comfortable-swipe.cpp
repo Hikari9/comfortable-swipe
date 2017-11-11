49a7eb4591e8ad418e7b571d19cf7d2d7943f53b
Rico Tiongson <thericotiongson@gmail.com>
2017-11-11 13:54:27 +0800

    [config] Change program location from ~/.local/bin to /usr/local/bin

--- src/comfortable-swipe.cpp
+++ src/comfortable-swipe.cpp
@@ -31,7 +31,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <ctime>
 #include <unistd.h>
 #define cstr const string&
-#define PROGRAM "$HOME/.local/bin/comfortable-swipe"
+#define PROGRAM "/usr/local/bin/comfortable-swipe"
 using namespace std;
 
 extern "C" {
