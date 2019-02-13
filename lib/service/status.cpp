8467b8239293a9403e2b7f4d79570d720a955cd8
Rico Tiongson <thericotiongson@gmail.com>
2019-02-13 10:01:30 +0800

    Update comment of status

--- lib/service/status.cpp
+++ lib/service/status.cpp
@@ -31,7 +31,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 namespace comfortable_swipe::service
 {
     /**
-     * Restarts the comfortable-swipe service.
+     * Prints the status of comfortable-swipe.
      */
     void status()
     {
