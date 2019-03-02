abef078c581c886ae839d77e116f7cafa25f1605
Rico Tiongson <thericotiongson@gmail.com>
2019-03-02 18:05:37 +0800

    Fix compilation for Python 3

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 import os
+import sys
 from setuptools import setup, find_packages
 from setuptools.extension import Extension
 
+__BIN__ = os.path.dirname(sys.executable)
+_SHARE_ = os.path.join(os.path.dirname(__BIN__), 'share')
 __CWD__ = os.getcwd()
 __DIR__ = os.path.abspath(os.path.dirname(__file__))
 __URL__ = 'https://github.com/Hikari9/comfortable-swipe-ubuntu'
@@ -9,8 +12,8 @@ __URL__ = 'https://github.com/Hikari9/comfortable-swipe-ubuntu'
 NAME = 'comfortable-swipe'
 PYTHON_NAME = NAME.replace('-', '_')
 VERSION = '1.1.0'
-PROGRAM = os.path.join('/usr/local/bin', NAME)
-CONFIG = os.path.join('/usr/local/share', NAME, NAME + '.conf')
+PROGRAM = os.path.join(__BIN__, NAME)
+CONFIG = os.path.join(_SHARE_, NAME, NAME + '.conf')
 
 # for C++ library
 cpp_macros = dict(
@@ -22,6 +25,10 @@ cpp_macros = dict(
 try:
     # make sure working directory is here
     os.chdir(__DIR__)
+
+    # make sure paths to program and config exist
+    os.makedirs(os.path.dirname(PROGRAM), exist_ok=True)
+    os.makedirs(os.path.dirname(CONFIG), exist_ok=True)
 
     # save README as long_description
     with open('README.md', 'r') as README_file:
@@ -52,6 +59,7 @@ try:
         author_email='thericotiongson@gmail.com',
         url=__URL__,
         packages=find_packages(),
+        scripts=['scripts/comfortable-swipe'],
         ext_modules=extensions
     )
 
