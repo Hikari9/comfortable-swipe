d73f125237d82ed713b765c04a41489cb43a52ce
Rico Tiongson <thericotiongson@gmail.com>
2019-03-03 23:03:40 +0800

    Error message

--- cpp/_macro.cpp
+++ cpp/_macro.cpp
@@ -1,12 +1,13 @@
 #ifndef __COMFORTABLE_SWIPE__macro_hpp__
 #define __COMFORTABLE_SWIPE__macro_hpp__
 
+#undef COMFORTABLE_SWIPE_CONFIG
 #ifndef COMFORTABLE_SWIPE_CONFIG
-#error "COMFORTABLE_SWIPE_CONFIG must be defined."
+#error COMFORTABLE_SWIPE_CONFIG must be defined.
 #endif /* COMFORTABLE_SWIPE_CONFIG */
 
 #ifndef COMFORTABLE_SWIPE_VERSION
-#warning "COMFORTABLE_SWIPE_VERSION is not defined."
+#warning COMFORTABLE_SWIPE_VERSION is not defined.
 #endif /* COMFORTABLE_SWIPE_VERSION */
 
 #endif /* __COMFORTABLE_SWIPE__macro_hpp__ */
