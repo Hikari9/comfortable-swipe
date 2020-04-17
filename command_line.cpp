2e91ac17fdb66141cc8612b2e51605d20ce3e445
Rico Tiongson <thericotiongson@gmail.com>
2020-04-18 07:20:53 +0800

    Prepare values for CMakeLists.txt

--- command_line.cpp
+++ command_line.cpp
@@ -16,7 +16,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "comfortable_swipe/lib"
+#include "comfortable_swipe/lib.cpp"
 #include <ios>      // std::ios
 #include <iostream> // std::cin, std::cout, std::cerr
 #include <string>   // std::string
